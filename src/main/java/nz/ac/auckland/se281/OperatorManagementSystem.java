5237210551f3cb5ebcf5555876f9e36bb2f65317
Tanush <tpan459@aucklanduni.ac.nz>
2025-03-30 14:09:35 +1300

    modified search operators and create operators and reordered code to fix heirachy issues. Passed test case 8,9,11

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -5,12 +5,68 @@ import nz.ac.auckland.se281.Types.Location;
 
 public class OperatorManagementSystem {
 
+  class Operator {
+    private String star;
+    private String name;
+    private String code;
+    private String location;
+
+    public Operator(String star, String name, String code, String location) {
+      this.star = star;
+      this.name = name;
+      this.code = code;
+      this.location = location;
+    }
+
+    public String getName() {
+      return this.name;
+    }
+
+    public String getCode() {
+      return this.code;
+    }
+
+    public String getLocation() {
+      return this.location;
+    }
+
+    public String getStar() {
+      return this.star;
+    }
+  }
+
+  ArrayList<Operator> operators = new ArrayList<>();
+
   // Do not change the parameters of the constructor
   public OperatorManagementSystem() {}
 
   public void searchOperators(String keyword) {
     keyword = keyword.trim().toLowerCase();
-    MessageCli.OPERATORS_FOUND.printMessage("are", "no", "s", ".");
+
+    int numOperatorsFound = 0;
+    ArrayList<Operator> foundOperators = new ArrayList<>();
+
+    for (int i = 0; i < operators.size(); i++) {
+      Operator current = operators.get(i);
+      if (operators.get(i).getName().toLowerCase().contains(keyword)
+          || operators.get(i).getCode().toLowerCase().contains(keyword)
+          || operators.get(i).getLocation().toLowerCase().contains(keyword)
+          || operators.get(i).getStar().toLowerCase().contains(keyword)) {
+        numOperatorsFound += 1;
+        foundOperators.add(current);
+      }
+    }
+    if (numOperatorsFound > 1) {
+      MessageCli.OPERATORS_FOUND.printMessage("are", String.valueOf(numOperatorsFound), "s", ":");
+    } else if (numOperatorsFound == 1) {
+      MessageCli.OPERATORS_FOUND.printMessage("is", String.valueOf(numOperatorsFound), "", ":");
+    } else {
+      MessageCli.OPERATORS_FOUND.printMessage("are", "no", "s", ".");
+    }
+
+    for (Operator op : foundOperators) {
+      MessageCli.OPERATOR_ENTRY.printMessage(op.getName(), op.getCode(), op.getLocation());
+    }
   }
 
   public void createOperator(String operatorName, String location) {
@@ -33,37 +89,6 @@ public class OperatorManagementSystem {
     MessageCli.OPERATOR_CREATED.printMessage(
         operatorName, initials.toString(), locationAsString); // prints the output
 
-    class Operator {
-      private String star;
-      private String name;
-      private String code;
-      private String location;
-
-      public Operator(String star, String name, String code, String location) {
-        this.star = star;
-        this.name = name;
-        this.code = code;
-        this.location = location;
-      }
-
-      public String getStar() {
-        return star;
-      }
-
-      public String getName() {
-        return name;
-      }
-
-      public String getCode() {
-        return code;
-      }
-
-      public String getLocation() {
-        return location;
-      }
-    }
-
-    ArrayList<Operator> operators = new ArrayList<>();
     operators.add(new Operator("* ", operatorName, initials.toString(), locationAsString));
   }
 
