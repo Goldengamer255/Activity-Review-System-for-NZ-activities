4e2d28fcb863b82d34b4dd50f81a6db49a497101
Tanush <tpan459@aucklanduni.ac.nz>
2025-04-15 22:26:45 +1200

    added methods for the review java class including an abstract method

--- src/main/java/nz/ac/auckland/se281/Review.java
+++ src/main/java/nz/ac/auckland/se281/Review.java
@@ -12,4 +12,22 @@ public abstract class Review {
     this.reviewerName = reviewerName;
     this.reviewId = reviewId;
   }
+
+  public String getReviewText() {
+    return reviewText;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public String getReviewId() {
+    return reviewId;
+  }
+
+  public String getReviewerName() {
+    return reviewerName;
+  }
+
+  public abstract void printReviewDetails();
 }
