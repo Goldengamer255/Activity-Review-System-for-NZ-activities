4605731a5b318b76dae3c4c291c66a910fac7223
Tanush <tpan459@aucklanduni.ac.nz>
2025-04-17 21:54:29 +1200

    Added new class for any expert reviews which extends review.java

---
+++ src/main/java/nz/ac/auckland/se281/ExpertReview.java
@@ -0,0 +1,36 @@
+package nz.ac.auckland.se281;
+
+public class ExpertReview extends Review {
+  private String email;
+  private boolean followUpRequested;
+  private String operatorResponse;
+
+  public ExpertReview(
+      String reviewerName, String email, int rating, String comments, boolean followUpRequested) {
+    super(comments, rating, reviewerName, null); // Pass null for reviewId if it's not needed
+    this.email = email;
+    this.followUpRequested = followUpRequested;
+    this.operatorResponse = null;
+  }
+
+  public void respond(String response) {
+    this.operatorResponse = response;
+  }
+
+  @Override
+  public void printReviewDetails() {
+    System.out.println("Expert Review by " + getReviewerName() + ":");
+    System.out.println("Email: " + email);
+    System.out.println("Rating: " + getRating());
+    System.out.println("Comments: " + getReviewText());
+    if (followUpRequested) {
+      if (operatorResponse != null) {
+        System.out.println("Operator Response: " + operatorResponse);
+      } else {
+        System.out.println("A reply is needed.");
+      }
+    } else {
+      System.out.println("This review is resolved.");
+    }
+  }
+}
